573337797c6a522f63bb0586c2cef928034dd043
Theophile-Kango <fadhili.kango@gmail.com>
2019-09-28 13:44:44 +0100

    Responsible design

--- script.js
+++ script.js
@@ -1,70 +1,91 @@
 const cards = document.querySelectorAll('.memory-card');
-let hasFlippedCard = false; 
+let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let count = 0, moved = 0;
-function flipCard (){
-    
-    document.querySelector('#moves').textContent = count;
-   
 
-    if(lockBoard) return;
-    if(this === firstCard) return;
+let seconds = 30;
+let minutes = 1;
+let zeroM = '0';
+let zeroS = '0';
+
+let temp = setInterval(() => {
+    //seconds++;
+    zeroM = minutes < 10 ? zeroM = '0' : '';
+    zeroS = seconds < 10 ? zeroS = '0' : '';
+    --seconds;
+    if ((minutes === 0) && (seconds === 0)) execute(); 
+    if(seconds == -1 ){
+        zeroS = '';
+        seconds = 59;
+        --minutes;
+    }
+   
+    document.querySelector('#time').textContent = `${zeroM}${minutes} : ${zeroS}${seconds}`;
+}, 1000);
+function flipCard() {
+
+    document.querySelector('#moves').textContent = count;
+
+
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
     this.classList.toggle('flip');
-    if (!hasFlippedCard){
+    if (!hasFlippedCard) {
         //first click
         hasFlippedCard = true;
-	count++;
+        count++;
         firstCard = this;
         return;
-    }else{
+    } else {
 
         //second click
         hasFlippedCard = false;
         secondCard = this;
         checkForMatch();
     }
-	
+    if (count > 20) execute();
 
 }
 
-const checkForMatch = () =>{
+const checkForMatch = () => {
     let isMatch = firstCard.dataset.frame === secondCard.dataset.frame;
-    
-    isMatch ? disableCards() : unflipCards(); 
-    if(isMatch){
- 	moved++;
-console.log(moved);
-  if(moved == 8){
-	let win = document.querySelector('.footer');
-	win.style.display = 'block';
+
+    isMatch ? disableCards() : unflipCards();
+    if (isMatch) {
+        moved++;
+
+        if (moved === 8) {
+            let win = document.querySelector('.footer');
+            win.style.display = 'block';
+            clearInterval(temp);
+        }
+
+    }
 
 }
-}
-  
-}
-    
 
-const disableCards = () =>{
+
+const disableCards = () => {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     resetBoard();
 }
-const unflipCards = () =>{
+const unflipCards = () => {
     lockBoard = true;
-    setTimeout(() =>{
+    setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         resetBoard();
     }, 1500);
 }
-const resetBoard = () =>{
+const resetBoard = () => {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
-const shuffle = () =>{
-    cards.forEach(card =>{
+const shuffle = () => {
+    cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 16);
         card.style.order = randomPos;
     });
@@ -72,37 +93,13 @@ const shuffle = () =>{
 shuffle();
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-const showTime = () =>{
-    let seconds = 0;
-    let minutes = 0;
-    let zeroM = '0';
-    let zeroS = '0';
-    
-        
-    setInterval(()=>{
-        seconds++;
-        if(minutes >= 10){
-	    zeroM = '';
-	}
-	if(seconds >= 10){
-	    zeroS = '';
-	}
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-        }
-        document.querySelector('#time').innerHTML = `${zeroM}${minutes} : ${zeroS}${seconds}`;
-    },1000);
-}
+
+
 let reset = document.querySelector('#reset');
-reset.addEventListener('click',execute);
-function execute(){
-    setTimeout(function(){
+reset.addEventListener('click', execute);
+function execute() {
+    setTimeout(function () {
         document.location.reload(true);
-     }, 100);
+    }, 100);
+
 }
-
-showTime();
-
-
-
